027965e471f992cfe703bdb36f6b599379012e6d
Raymond Kao <ray@peopleandcode.com>
2015-05-14 21:34:39 -0400

    moved styles and js folder from editor to root repo

--- editor/footer.php
+++
@@ -1,6 +0,0 @@
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/jquery-ui.js"></script>
-    <script type="text/javascript" src="js/editor.js"></script>
-
-  </body>
-</html>
--- editor/top_menu.php
+++
@@ -1,3 +0,0 @@
-<nav id="top_menu">
-  <span id="hamburger">Menu</span>
-</nav>
